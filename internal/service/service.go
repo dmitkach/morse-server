6e4ffff848ab3e0ee30771652111f989ff29ae15
Dmitry Tkach <dmitkach@icloud.com>
2026-02-03 16:47:28 +0300

    - func TextTypeSwitch is done

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"morse-server/pkg/morse"
+	"unicode"
+)
+
+func TextTypeSwitch(s string) string {
+	var isMorse = true
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			isMorse = false
+			break
+		}
+	}
+
+	var res string
+	if isMorse {
+		res = morse.ToText(s)
+	} else {
+		res = morse.ToMorse(s)
+	}
+
+	return res
+}
